ff3f6c38d096f42df07b577d46e297d9dc99bf8d
Erick Duarte <erickod@gmail.com>
2023-10-20 07:25:13 -0300

    test: add FakeHttpClient as test helper

---
+++ tests/helpers/fake_http_client.py
@@ -0,0 +1,31 @@
+from typing import Any
+
+
+class DummyResponse:
+    def __init__(self, response_data: dict[str, Any], *args, **kwargs) -> None:
+        self.response_data = response_data
+
+    def json(self) -> dict[str, Any]:
+        return self.response_data
+
+
+class FakeHttpClient:
+    def __init__(
+        self,
+        get_output: dict[str, Any] = {},
+        post_output: dict[str, Any] = {},
+        response_class: Any = DummyResponse,
+    ) -> None:
+        self.get_output = get_output
+        self.post_output = post_output
+        self.get_is_called: bool = False
+        self.post_is_called: bool = False
+        self.response_class = response_class
+
+    def get(self, *args, **kwargs) -> Any:
+        self.get_is_called = True
+        return self.response_class(response_data=self.get_output)
+
+    def post(self, *args, **kwargs) -> Any:
+        self.post_is_called = True
+        return self.response_class(response_data=self.post_output)
