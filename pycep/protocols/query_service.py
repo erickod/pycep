391971f10d3e63f215227d856ad8d0aa4e955d9f
Erick Duarte <erickod@gmail.com>
2023-10-21 18:39:33 -0300

    refactor: QueryService protocol must returns CepData

--- pycep/protocols/query_service.py
+++ pycep/protocols/query_service.py
@@ -1,7 +1,9 @@
-from typing import Any, Protocol, runtime_checkable
+from typing import Protocol, runtime_checkable
+
+from pycep.cep_data import CepData
 
 
 @runtime_checkable
 class QueryService(Protocol):
-    async def query_cep(self, cep: str) -> Any:
+    async def query_cep(self, cep: str) -> CepData:
         pass
