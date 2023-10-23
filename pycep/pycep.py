ac37df69906927227836ec81098885f909408b6b
Erick Duarte <erickod@gmail.com>
2023-10-23 07:35:29 -0300

    refactor: CepFactory and method calls

--- pycep/pycep.py
+++ pycep/pycep.py
@@ -16,27 +16,28 @@ class PyCEP:
         self.__tasks: list[asyncio.Task] = []
         self.__cep_data: CepData | None = None
         self.__status: str = "waiting_query"
-        self.__services and asyncio.run(self.__query_services(cep))
+        asyncio.run(self.__query_services(cep))
 
-    async def __create_tasks(self, cep: str) -> Any:
+    def __create_tasks(self, cep: str) -> Any:
         for service in self.__services:
             task = self.__async_runner.create_task(service.query_cep(cep))
             self.__tasks.append(task)
 
     async def __query_services(self, cep: str) -> Any:
-        await self.__create_tasks(cep)
+        self.__status = "querying"
+        self.__create_tasks(cep)
         self.__tasks and await asyncio.wait(
             self.__tasks, return_when=asyncio.FIRST_COMPLETED
         )
-        self.__tasks and await self.__cancel_pending_tasks()
+        self.__cancel_pending_tasks()
         self.__status = "query_done"
 
-    async def __cancel_pending_tasks(self) -> None:
+    def __cancel_pending_tasks(self) -> None:
         for task in self.__tasks:
-            task.done() and await self.__configure_cep_data(task)
+            task.done() and self.__configure_cep_data(task)
             not task.done() and task.cancel()
 
-    async def __configure_cep_data(self, task: asyncio.Task) -> None:
+    def __configure_cep_data(self, task: asyncio.Task) -> None:
         if self.__cep_data:
             return
         self.__cep_data = task.result()
@@ -87,7 +88,8 @@ class CepFactory:
     ) -> PyCEP:
         return PyCEP(
             cep=cep,
-            cep_services_loader=CepQueryServiceLoader(module=services),
+            cep_services_loader=cep_services_loader
+            or CepQueryServiceLoader(module=services),
         )
 
 
