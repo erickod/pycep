6c5314eeecf23662e8c232f41d4963dddd461791
Erick Duarte <erickod@gmail.com>
2023-11-01 14:14:56 -0300

    feat: open

--- pycep/services/opencep.py
+++ pycep/services/opencep.py
@@ -10,6 +10,7 @@ class OpenCepService:
         self.__http_client = http_client
 
     async def query_cep(self, cep: str) -> CepData:
+        cep = cep.replace("-", "").replace(".", "")
         response = await self.__http_client.get(self.__base_url.format(cep=cep))
         response_asdict = response.json()
         return CepData(
