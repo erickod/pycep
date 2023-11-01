bdc527c4ad7b2c9d9c974cefc837916225fefa1e
Erick Duarte <erickod@gmail.com>
2023-11-01 13:14:14 -0300

    test: reponse when awaiting json has side effects

--- tests/test_aiohttp_client.py
+++ tests/test_aiohttp_client.py
@@ -1,11 +1,14 @@
+from typing import Callable
 from unittest.mock import AsyncMock
+
+import aiohttp
 
 from pycep.adapters.aiohttp_client import AioHttpHttpClient, HttpResponse
 
 
 class FakeAioHttp:
-    def __init__(self, *, output_value) -> None:
-        self._amock = AsyncMock()
+    def __init__(self, *, output_value, async_mock: AsyncMock = AsyncMock()) -> None:
+        self._amock = async_mock
         self._amock.__aenter__.return_value = self._amock
         self._amock.get.return_value = self._amock
         self._amock.post.return_value = self._amock
@@ -54,3 +57,16 @@ async def test_post_method_with_no_input() -> None:
     output = await sut.post("https://acme.co")
     assert isinstance(output, HttpResponse)
     assert output.json() == client.output_value
+
+
+async def test_reponse_when_awaiting_json_has_side_effects() -> None:
+    def side_effect(*args, **kwargs):
+        raise aiohttp.client_exceptions.ContentTypeError({}, None)
+
+    async_mock = AsyncMock()
+    async_mock.json.side_effect = side_effect
+    client = FakeAioHttp(output_value="any valid output", async_mock=async_mock)
+    sut = AioHttpHttpClient(client=client)
+    output = await sut.post("https://acme.co")
+    assert isinstance(output, HttpResponse)
+    assert output.text() == client.output_value
