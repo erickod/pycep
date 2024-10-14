de494b88d5d0d1ba1689eabea051671408ba391d
Erick Duarte <erickod@gmail.com>
2024-10-14 09:02:19 -0300

    refactor: add status attr to the FakeHttpClient

--- tests/helpers/fake_http_client.py
+++ tests/helpers/fake_http_client.py
@@ -8,20 +8,28 @@ class FakeHttpClient:
         self,
         get_output: dict[str, Any] | str | bytes = {},
         post_output: dict[str, Any] | str | bytes = {},
+        status_output: int = 200,
         response_class: Any = HttpResponse,
     ) -> None:
         self.get_output = get_output
         self.post_output = post_output
         self.get_is_called: bool = False
         self.post_is_called: bool = False
+        self.status_output = status_output
         self.response_class = response_class
 
     async def get(self, *args, **kwargs) -> Any:
         self.get_is_called = True
-        return self.response_class(json_data=self.get_output, text_data=self.get_output)
+        return self.response_class(
+            status=self.status_output,
+            json_data=self.get_output,
+            text_data=self.get_output
+        )
 
     async def post(self, *args, **kwargs) -> Any:
         self.post_is_called = True
         return self.response_class(
-            json_data=self.post_output, text_data=self.post_output
+            status=self.status_output,
+            json_data=self.post_output,
+            text_data=self.post_output
         )
