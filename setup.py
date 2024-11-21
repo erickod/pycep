17a354644faa4b691cf3efdd6a1ff30306abe8b2
Erick Duarte <erickod@gmail.com>
2024-11-21 16:55:10 -0300

    fix: use find_packages on setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="pycep",
@@ -6,7 +6,7 @@ setup(
     description="Consulta CEPs em vários serviços (Correios, ViaCep, OpenCep) de maneira totalmente assíncrona",
     author="Erick Duarte",
     author_email="erickod@gmail.com",
-    packages=["pycep"],  # same as name
+    packages=find_packages(),
     install_requires=[
         "wheel",
         "httpx==0.24.1",
